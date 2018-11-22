f04d2c57cf8c55f0bddc6c64adf87524dc969464
nuryagdym <26792980+nuryagdym@users.noreply.github.com>
2018-11-22 18:38:42 +0300

    Update activate.tpl.php

--- views/auth/email/activate.tpl.php
+++ views/auth/email/activate.tpl.php
@@ -3,6 +3,6 @@
 	<h3><?php echo sprintf(lang('email_activate_heading'), $identity);?></h3>
 	<p><?php echo sprintf(lang('email_activate_subheading'), anchor('auth/activate/'. $id .'/'. $activation, lang('email_activate_link')));?></p>
 	<br>
-	<p><?php echo sprintf(lang('email_activate_message'), base_url('auth/activate/'. $id .'/'. $activation, lang('email_activate_link')));?></p>
+	<p><?php echo sprintf(lang('email_activate_message'), base_url('auth/activate/'. $id .'/'. $activation));?></p>
 </body>
 </html>
