36b8ff32eac909eda8e5764911b6fe43ff03074c
nuryagdym <26792980+nuryagdym@users.noreply.github.com>
2018-11-22 18:33:59 +0300

    added Message for plain text activation url

--- views/auth/email/activate.tpl.php
+++ views/auth/email/activate.tpl.php
@@ -1,6 +1,8 @@
 <html>
 <body>
-	<h1><?php echo sprintf(lang('email_activate_heading'), $identity);?></h1>
+	<h3><?php echo sprintf(lang('email_activate_heading'), $identity);?></h3>
 	<p><?php echo sprintf(lang('email_activate_subheading'), anchor('auth/activate/'. $id .'/'. $activation, lang('email_activate_link')));?></p>
+	<br>
+	<p><?php echo sprintf(lang('email_activate_message'), base_url('auth/activate/'. $id .'/'. $activation, lang('email_activate_link')));?></p>
 </body>
 </html>
